6636690c0a40539686518a113baa8a7bc7ba3cf2
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-02-21 18:28:41 -0300

    Configura ThemeContext e _app.tsx para gerenciar Dark/Light Mode globalmente

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { AppProps } from "next/app"
-import { useEffect, useState } from "react"
+import { ThemeProvider } from "../context/ThemeContext"
 import "../styles/globals.scss"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState("light")
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme")
-    if (storedTheme) {
-      setTheme(storedTheme)
-      document.documentElement.setAttribute("data-theme", storedTheme)
-    }
-  }, [])
-
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
